ec42a7cd5a9931a5be8c97971a57b94a4993b9f8
CJ <chaojin101@gmail.com>
2023-05-12 10:49:17 +0800

    new example

---
+++ 2023-05-12-Two-dimensional-img-layout/grid/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container">
+    <div class="img_container" v-for="i in 7">
+      <img :src="`${i}.jpg`" alt="">
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  gap: 4px;
+}
+img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
