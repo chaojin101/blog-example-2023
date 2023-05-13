f1eb744cb949c08bc973bf5f0dbc6db27dd9d790
CJ <chaojin101@gmail.com>
2023-05-13 09:34:51 +0800

    fix

--- 2023-05-12-Two-dimensional-img-layout/wookmark/src/App.vue
+++ 2023-05-12-Two-dimensional-img-layout/wookmark/src/App.vue
@@ -1,47 +1,86 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted, computed } from 'vue';
+const getMinItemIndex = (arr: number[]) => {
+  let minItemIndex = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[minItemIndex]) {
+      minItemIndex = i;
+    }
+  }
+  return minItemIndex;
+};
+
+const imgAmount = ref(8);
+const containerHeight = ref(1000); // will be caculated after img loaded
+const colAmount = ref(3);
+const gap = ref(4);
+const colWidth = computed(
+  () =>
+    (document.body.offsetWidth - gap.value * (colAmount.value + 1)) /
+    colAmount.value
+);
+const imgRefs = ref<HTMLImageElement[]>([]);
+const imgContainerStyles = ref<any[]>(new Array(imgAmount.value)); // calculated by imgRefs
+
+const waitImgsLoaded = async () => {
+  // wait for all the img loaded to get the right img height
+  const promises = [];
+  for (let i = 0; i < imgAmount.value; i++) {
+    const promise = new Promise<void>((resolve) => {
+      const listener = () => {
+        imgRefs.value[i].removeEventListener('load', listener);
+        resolve();
+      };
+      imgRefs.value[i].addEventListener('load', listener);
+    });
+    promises.push(promise);
+  }
+  await Promise.all(promises);
+};
+
+const setWookmark = async () => {
+  await waitImgsLoaded();
+
+  // caculate position
+  const colHeights = new Array(colAmount.value).fill(0);
+  for (let i = 0; i < imgAmount.value; i++) {
+    const minHeightColIndex = getMinItemIndex(colHeights);
+    const left = gap.value + minHeightColIndex * (gap.value + colWidth.value);
+    const top = colHeights[minHeightColIndex] + gap.value;
+    imgContainerStyles.value[i] = {
+      position: 'absolute',
+      left: left + 'px',
+      top: top + 'px',
+    };
+    colHeights[minHeightColIndex] += gap.value + imgRefs.value[i].offsetHeight;
+  }
+
+  containerHeight.value = Math.max(...colHeights);
+};
+onMounted(async () => {
+  await setWookmark();
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div :style="{ height: containerHeight + 'px' }" class="container">
+    <div
+    v-for="i in imgAmount"
+    class="img_container"
+    :style="{ width: colWidth + 'px', ...imgContainerStyles[i - 1] }"
+    >
+    <!-- this i starts at 1 -->
+      <img ref="imgRefs" :src="`${i}.jpg`" alt="" />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  position: relative;
 }
-
-.logo {
+img {
   display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  width: 100%;
 }
 </style>
